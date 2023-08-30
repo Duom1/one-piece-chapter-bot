d08921688e00dc158adc03c7f5d41b7572ebfd5c
Your Name <you@example.com>
2023-08-30 19:54:17 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# one piece chapter bot
+### a discord web hook bot that send a message when a new chapter is up on magareader.to
+add a ".env" file where you write "DISCORD_WEBHOOK_URL=your url".
+then install the required libraries from requirements.txt or run create_env.sh
---
+++ main.py
@@ -0,0 +1,59 @@
+import requests, os
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+def main():
+    def store_number(number: int):
+        with open("chapter.txt", "w") as file:
+            file.write(str(number))
+
+    def read_number()->int:
+        number = 0;
+        with open("chapter.txt", "r") as file:
+            number = file.read()
+        return int(number)
+
+    print("getting content")
+    url = "https://mangareader.to/one-piece-3"
+    response = requests.get(url)
+    html_content = response.content
+
+    print("parsing html")
+    soup = BeautifulSoup(html_content, "html.parser")
+    element_amt = 10
+    count = 0
+    target_content = []
+    first_element = soup.find(class_="item reading-item chapter-item")
+    current_element = first_element
+    while current_element and count < element_amt:
+        content = current_element.get_text()
+        modified_content = content[3:-9]
+        target_content.append(modified_content)
+        count += 1
+        current_element = current_element.find_next(class_="item reading-item chapter-item")
+
+    chapter_numbers = []
+    for i in target_content:
+        item = i[8:]
+        chapter_numbers.append(int(item[:4]))
+
+    print("sending webhook (if needed)")
+    newest_chapter = max(chapter_numbers)
+    if newest_chapter > read_number():
+        store_number(newest_chapter)
+        load_dotenv()
+        webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
+        if webhook_url is None:
+            print("Webhook URL not found in environment variables.")
+        else:
+            content = f'@everyone NEW ONEPICE CHAPTER "{{}}" IS OUT'.format(target_content[0][8:])
+            message_data = { 'content': content }
+            response = requests.post(webhook_url, json=message_data)
+            if response.status_code == 204:
+                print('Webhook sent successfully!')
+            else:
+                print('Failed to send webhook. Status code:', response.status_code)
+                print('Response:', response.text)
+
+if __name__ == "__main__":
+    main()
